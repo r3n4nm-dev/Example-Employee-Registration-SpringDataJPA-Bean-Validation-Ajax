a2049d32004ccc824b7da1835a08495ef72458d2
r3n4nm-dev <85502192+r3n4nm-dev@users.noreply.github.com>
2021-07-19 20:20:45 -0300

    Add files via upload

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into funcao (funcaoid, nome_funcao) values (1, 'Administrador');
+insert into funcao (funcaoid, nome_funcao) values (2, 'Supervisor');
+insert into funcao (funcaoid, nome_funcao) values (3, 'Funcionario');
